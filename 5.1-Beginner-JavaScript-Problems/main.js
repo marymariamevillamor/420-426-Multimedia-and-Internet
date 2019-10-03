7acff9c78c77e5a2edd267757cc0bb5712af494e
Mary Mariame Villamor <1678779@johnabbottcollege.net>
2019-10-03 10:31:12 -0400

    2. Say Hi

--- 5.1-Beginner-JavaScript-Problems/main.js
+++ 5.1-Beginner-JavaScript-Problems/main.js
@@ -2,3 +2,8 @@ function multiplyTwoNumbers(num1, num2) {
     return result = num1 * num2;
 }
 
+function sayHi(name, age) {
+    let salutation = "Hi. My name is " + name + " and I'm " + age + " years old."
+    return salutation; 
+}
+
