29a1ee546de62b5df38f79f23f11932528e4a3c1
Mary Mariame Villamor <1678779@johnabbottcollege.net>
2019-10-03 11:21:05 -0400

    4. Fizz Buzz

--- 5.1-Beginner-JavaScript-Problems/main.js
+++ 5.1-Beginner-JavaScript-Problems/main.js
@@ -19,3 +19,18 @@ function indexPower(array, n) {
         }
     }
 }
+
+function fizzBuzz(number) {
+    if(number % 3 == 0 && number % 5 == 0) {
+        return "Fizz Buzz"
+    }
+    else if(number % 3 == 0) {
+        return "Fizz"
+    }
+    else if(number % 5 == 0) {
+        return "Buzz"
+    }
+    else {
+        return number;
+    }
+}
