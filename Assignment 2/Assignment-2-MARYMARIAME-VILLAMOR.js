fd1fc8882242e84559eb8b7e19ec2aa3a3bfd5da
Mary Mariame Villamor <1678779@johnabbottcollege.net>
2019-10-22 22:41:04 -0400

    1. Tic-Tac-Toe Referee
    finished

--- Assignment 2/Assignment-2-MARYMARIAME-VILLAMOR.js
+++ Assignment 2/Assignment-2-MARYMARIAME-VILLAMOR.js
@@ -4,21 +4,33 @@ function xoReferee(firstRow, secondRow, thirdRow) {
         let newValue = stringIt.split('');
         return newValue;
     }
-    function checkResult(theArray) {
-        //check by row
-        for (let i = 0; i < gridSize; i++) {
-            if (theArray[i][0] == theArray[i][1] == theArray[i][2]) {
-                console.log("THERES A WINNER!");
+    function checkWinner(a) {
+        let counter = 0;
+        while (counter < a.length) {
+            if (a[counter][0] == a[counter][1] && 
+                a[counter][1] == a[counter][2]) {
+                    return a[counter][counter];
+            }
+            else if (a[0][counter] == a[1][counter] &&
+                a[2][counter]) {
+                    return a[0][counter];
+            }
+            else if (a[0][0] == a[1][1] && a[1][1] == a[2][2] ||
+                a[0][2] == a[1][1] && a[1][1] == a[2][0]) {
+                    return a[1][1];
+            }
+            else {
+                counter += 1;
             }
         }
+        return "D";
     }
-    let gridSize = 3;
-    let winner = false;
     
     let arr1 = splitString(firstRow);
     let arr2 = splitString(secondRow);
     let arr3 = splitString(thirdRow);
-    let bigArr = [arr1, arr2, arr3];
-    checkResult(bigArr);
+    let theArray = [arr1, arr2, arr3];
+    
+    console.log(checkWinner(theArray));
 }
 
