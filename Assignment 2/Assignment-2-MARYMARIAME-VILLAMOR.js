83944356126bd765a01e69f35c82a3a0e11b9ef2
Mary Mariame Villamor <1678779@johnabbottcollege.net>
2019-10-24 09:16:27 -0400

    Assignment 2
    skipped #2, doing #3

--- Assignment 2/Assignment-2-MARYMARIAME-VILLAMOR.js
+++ Assignment 2/Assignment-2-MARYMARIAME-VILLAMOR.js
@@ -34,3 +34,38 @@ function xoReferee(firstRow, secondRow, thirdRow) {
     console.log(checkWinner(theArray));
 }
 
+//TO DO - ASK FOR HELP
+function safePawns (coordinates) {
+    function splitString(value) {
+        let stringIt = value.toString();
+        let newValue = stringIt.split(',');
+        return newValue;
+    }
+    function splitMeAgain(value) {
+        let stringIt = value.toString();
+        let newValue = stringIt.split('');
+        return newValue;
+    }
+
+    function byIndex(a, rows, columns) {
+        for (let i = 0; i < a.length; i++) {
+            let indexVal = splitMeAgain(a[i]);
+
+
+        }
+    }
+
+    let ranks = [1, 2, 3, 4, 5, 6, 7, 8];
+    let files = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    let cString = splitString(coordinates);
+
+    let runThis = byIndex(cString, ranks, files);
+    console.log(runThis);
+}
+
+function rectagnlesUnion () {
+    
+}
+
+
+
