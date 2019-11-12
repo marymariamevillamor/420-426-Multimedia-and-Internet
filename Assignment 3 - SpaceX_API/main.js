f7950385dce8b6846399cbcdf6a039629676020f
Mary Mariame Villamor <1678779@johnabbottcollege.net>
2019-11-11 23:34:20 -0500

    Assignment 3 round buttons + fetch url

--- Assignment 3 - SpaceX_API/main.js
+++ Assignment 3 - SpaceX_API/main.js
@@ -1,24 +1,25 @@
-function openPage(pageName) {
-    var i;
-    var x = document.getElementsByClassName("menuPage");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
-    }
-    document.getElementById(pageName).style.display = "block";  
-  }
+const url = "https://api.spacexdata.com/v3/";
 
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    doStuff(data)
+  })
 
-  const url = "https://api.spacexdata.com/v3/";
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      doStuff(data)
-    })
-    .catch (function(error) {
-      console.log(error);
-    });
+  .catch(function(error) {
+    console.log(error);
+  });
 
 function doStuff(data) {
   console.log(data);
+}
+
+function openPage(pageName) {
+  let i;
+  let x = document.getElementsByClassName("menuPage");
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(pageName).style.display = "block";  
 }
