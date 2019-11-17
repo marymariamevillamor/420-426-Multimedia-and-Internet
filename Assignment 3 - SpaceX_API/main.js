c40aa84b1a2567a60b78244d790f52176e1d3971
Mary Mariame Villamor <1678779@johnabbottcollege.net>
2019-11-17 13:52:06 -0500

    Assignment 3 - Finished

--- Assignment 3 - SpaceX_API/main.js
+++ Assignment 3 - SpaceX_API/main.js
@@ -7,7 +7,7 @@ function openPage(pageName) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  document.getElementById(pageName).style.display = "block";  
+  document.getElementById(pageName).style.display = "flex";  
 }
 
 function Didyouknow() {
